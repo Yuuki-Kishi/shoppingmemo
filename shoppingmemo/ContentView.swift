ab9f0a8f88d53ee7f6601ad89b1052ad5b6dd817
Yuuki-Kishi <yuuki.k.2006@gmail.com>
2025-08-25 02:54:33 +0900

    Initial Commit

---
+++ shoppingmemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  shoppingmemo
+//
+//  Created by 岸　優樹 on 2025/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ shoppingmemo/shoppingmemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  shoppingmemoApp.swift
+//  shoppingmemo
+//
+//  Created by 岸　優樹 on 2025/08/25.
+//
+
+import SwiftUI
+
+@main
+struct shoppingmemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
